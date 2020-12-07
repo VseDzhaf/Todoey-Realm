8429b049999cc783e1cae75a0a22c6dd08d95464
Vsevolod Dzhafarov <sevachok7@gmail.com>
2020-12-07 15:49:32 +0300

    Pushing commit

---
+++ Todoey/Model/Colours Constaint.swift
@@ -0,0 +1,14 @@
+//
+//  Colours Constaint.swift
+//  Todoey
+//
+//  Created by Vsevolod on 03.12.2020.
+//
+import UIKit
+import Foundation
+
+struct Colours {
+    var main: UIColor = .white
+    var contrast: UIColor = .white
+    var
+}
